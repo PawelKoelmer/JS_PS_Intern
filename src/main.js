f239c4fc61497ef158d434e0c620bab1db40a6a1
PawelKoelm <pawelkoelm.90@gmail.com>
2022-03-03 20:40:04 +0100

    Po zmianach

--- src/main.js
+++ src/main.js
@@ -1,199 +1,286 @@
 const API_URL =
-  "https://randomuser.me/api/?inc=gender,first,name,nat,location,registered,picture&noinfo";
+  'https://randomuser.me/api/?inc=gender,first,name,nat,location,registered,picture&noinfo';
 const localStorageKeys = {
-  USERS_TABLE: "usersTable",
+  USERS_TABLE: 'usersTable',
 };
+const classNames = {
+  CONTAINER: 'container',
+  BUTTON: 'generate-button',
+  PERSON_DATA: 'person-data',
+  FIRST_NAME: 'firstName',
+  LAST_NAME: 'lastName',
+  NATIONALITY: 'nationality',
+  REGISTER_DATE: 'registerDate',
+  ADDRESS: 'locationAddress',
+  CHECKBOX: 'address-checkbox',
+  ICON_LIST: 'icons-list'
+};
+
+const fieldsTable = ['firstName','lastName','locationAddress','nationality','registerDate'];
+
+//INITIALIZE
 
 main();
 
 function main() {
-  createPersonDiv();
-  createPersonDataBody();
-  generateButton();
-  generateLinkToTable();
-  generateCheckBox();
+  appendElementToBody(createPersonContainer());
+  appendElement(createPersonDataBody(),classNames.CONTAINER);
+  appendElement(generateButton(),classNames.CONTAINER);
+  appendElementToBody(createLinkToTable());
+  appendElement(createIconsListContainer(),classNames.CONTAINER);
 }
+
+//FETCH PERSON
 
 async function generatePerson() {
   try {
-    const person = await getData();
+    const person = await fetchPerson();
     addCreatedPersonToList(person);
-    createPerson(person);
+    appendPersonToBody(person);
   } catch (error) {
     console.log(error);
   }
 }
 
-function getData() {
-  return fetch(API_URL)
-    .then((res) => res.json())
-    .then((data) => {
-      const person = data.results[0];
-      const createdPerson = createPersonFromData(person);
-      return createdPerson;
-    });
-}
-
-function createPerson(createdPerson) {
+async function fetchPerson() {
   try {
-    document.querySelector(".person-data").remove();
-    createPersonDataBody();
-    addFieldsToPersonData(createdPerson);
+    const person = await fetch(API_URL).then((res) => res.json());
+    return createPersonFromData(person.results[0]);
   } catch (error) {
-    addFieldsToPersonData(createdPerson);
+    console.log('Failed to receive API data');
   }
 }
 
-function addImage() {}
-
-function addField(fieldName, param) {
-  let field = document.createElement("div");
-  field.className = fieldName;
-  field.innerHTML = param;
-  document.querySelector(".person-data").appendChild(field);
+function createPersonFromData(person) {
+  try {
+    return {
+      firstName: person.name.first,
+      lastName: person.name.last,
+      registerDate: Date.parse(person.registered.date),
+      nationality: person.nat,
+      locationAddress: {
+        country: person.location.country,
+        city: person.location.city,
+        state: person.location.state,
+        postcode: person.location.postcode,
+        street: person.location.street.name,
+        houseNumber: person.location.street.number,
+      },
+      picture: {
+        large: person.picture.large,
+        medium: person.picture.medium,
+        thumbnail: person.picture.thumbnail,
+      },
+    };
+  } catch (error) {
+    console.log('Person is invalid');
+  }
 }
 
-function generateCheckBox() {
-  let checkBox = document.createElement("input");
-  checkBox.type = "checkbox";
-  checkBox.className = "adress-checkbox";
-  checkBox.onclick = checkCheckbox;
+function addCreatedPersonToList(person) {
+  try {
+    localStorage.setItem(
+      localStorageKeys.USERS_TABLE,
+      JSON.stringify([...readUsers(), person])
+    );
+  } catch (error) {
+    console.log('Cannot add generated user to table');
+  }
+}
+
+
+
+
+
+
+//DISPLAY ELEMENTS
+
+function appendPersonToBody(createdPerson) {
+  try {
+    getByClassName(classNames.PERSON_DATA).remove();
+    appendElement(createPersonDataBody(),classNames.CONTAINER);
+    appendFieldsToPersonData(createdPerson);
+    appendElement(createAddressCheckBox(),classNames.PERSON_DATA);
+    appendElement(createLabelToCheckBox(),classNames.PERSON_DATA);
+  } catch (error) {
+    appendFieldsToPersonData(createdPerson);
+    appendElement(createAddressCheckBox(),classNames.PERSON_DATA);
+    appendElement(createLabelToCheckBox(),classNames.PERSON_DATA);
+  }
+}
+
+
+
+
+
+//CREATE ELEMENTS
+
+function createPersonContainer() {
+  const personContainer = document.createElement('div');
+  personContainer.className = classNames.CONTAINER;
+  return personContainer;
+}
+
+function createPersonDataBody() {
+  const personDataBody = document.createElement('div');
+  personDataBody.className = classNames.PERSON_DATA;
+  return personDataBody;
+}
+
+function createField(fieldClass, fieldText) {
+  const field = document.createElement('div');
+  field.className = fieldClass;
+  if(fieldClass === classNames.REGISTER_DATE){
+    field.innerHTML = timestampToDate(fieldText);
+  }
+  else if(fieldClass === classNames.ADDRESS){
+    let text = '';
+    for(const position in fieldText){
+      text += `${fieldText[position]}<br>`;
+    }
+    field.innerHTML = text;
+  }
+  else{
+    field.innerHTML = fieldText;
+  }
+  return field;
+}
+
+function createAddressCheckBox() {
+  const checkBox = document.createElement('input');
+  checkBox.id = 'check';
+  checkBox.type = 'checkbox';
+  checkBox.className = classNames.CHECKBOX;
+  checkBox.addEventListener('click', isAddressChecked);
   checkBox.checked = true;
-  document.body.appendChild(checkBox);
+  return checkBox;
+}
+
+function createLabelToCheckBox() {
+  const label = document.createElement('label');
+  label.htmlFor = 'check';
+  label.appendChild(document.createTextNode('Show/Hide Address'));
+  return label;
 }
 
 function generateButton() {
-  let button = document.createElement("button");
-  button.addEventListener("click", generatePerson);
-  button.textContent = "Generate User";
-  button.setAttribute("class", "generate-button");
-  document.body.appendChild(button);
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.addEventListener('click', generatePerson);
+  button.textContent = 'Generate User';
+  button.className = classNames.BUTTON;
+  return button;
 }
 
-function checkCheckbox() {
-  if (document.getElementsByClassName("adress-checkbox")[0].checked) {
-    document.getElementsByClassName("address")[0].style.display = "block";
-  } else document.getElementsByClassName("address")[0].style.display = "none";
+function isAddressChecked() {
+  if(getByClassName(classNames.CHECKBOX).checked){
+    getByClassName(classNames.ADDRESS).style.display = 'block';
+  } else{
+    getByClassName(classNames.ADDRESS).style.display = 'none';
+  } 
 }
 
-function generateLinkToTable() {
-  let link = document.createElement("a");
-  let linkText = document.createTextNode("Tabela użytkowników");
+function createIconsListContainer() {
+  const ul = document.createElement('ul');
+  ul.className = 'icons-list';
+  return ul;
+}
+
+/* function createIcon(fieldName) {
+  const li = document.createElement('li');
+  li.innerHTML = fieldName;
+  return li;
+} */
+
+function createLinkToTable() {
+  const link = document.createElement('a');
+  const linkText = document.createTextNode('Show last 10 registered users');
   link.appendChild(linkText);
-  link.onclick = openPage;
-  link.title = "Tabela użytkowników";
-  link.href = "showRegisteredUsers.html";
-  document.body.appendChild(link);
+  link.addEventListener('click', openPage);
+  link.href = 'showRegisteredUsers.html';
+  return link;
+}
+
+function saveTableToSession(tableToSession) {
+  try {
+    sessionStorage.setItem(
+      localStorageKeys.USERS_TABLE,
+      JSON.stringify(tableToSession)
+    );
+  } catch (error) {
+    console.log('Cannot stringify data to session');
+  }
 }
 
 async function openPage() {
-  const tableTosession = await sendTableToSesion();
+  await sendTableToSession();
 }
 
-function sendTableToSesion() {
+function sendTableToSession() {
   const registeredUsers = readUsers();
   return new Promise(function (resolve, reject) {
-    if (registeredUsers.length > 10) {
-      tableToSession = registeredUsers.slice(registeredUsers.length - 10);
-      addTableToSession(tableToSession);
-    } else {
-      tableToSession = registeredUsers;
-      addTableToSession(tableToSession);
+    try {
+      if (registeredUsers.length > 10) {
+        saveTableToSession(registeredUsers.slice(registeredUsers.length - 10));
+      } else {
+        saveTableToSession(registeredUsers);
+      }
+    } catch (error) {
+      reject(console.log('Problems with sending data'));
     }
   });
-}
-
-function timestampToDate(timestamp) {
-  let date = new Date(timestamp);
-  return (
-    date.getDate() +
-    "/" +
-    (date.getMonth() + 1) +
-    "/" +
-    date.getFullYear() +
-    " " +
-    date.getHours() +
-    ":" +
-    date.getMinutes() +
-    ":" +
-    date.getSeconds()
-  );
 }
 
 function readUsers() {
   return readLocalStorageByKey(localStorageKeys.USERS_TABLE);
 }
 
+function appendFieldsToPersonData(createdPerson) {
+  for(const field in fieldsTable){
+    getByClassName(classNames.PERSON_DATA).appendChild(createField(fieldsTable[field],createdPerson[fieldsTable[field]]));
+  }
+}
+
+
+
+
+
+//HELPERS
+
 function readLocalStorageByKey(key) {
   try {
     const users = localStorage.getItem(key);
     return users ? JSON.parse(users) : [];
   } catch (error) {
-    console.log("Failed to read users", error);
+    console.log('Failed to read users', error);
   }
 }
 
-function addTableToSession(tableToSession) {
-  console.log(tableToSession);
-  sessionStorage.setItem(
-    localStorageKeys.USERS_TABLE,
-    JSON.stringify(tableToSession)
+function timestampToDate(timestamp) {
+  const date = new Date(timestamp);
+  return (
+    date.getDate() +
+    '/' +
+    (date.getMonth() + 1) +
+    '/' +
+    date.getFullYear() +
+    ' ' +
+    date.getHours() +
+    ':' +
+    date.getMinutes() +
+    ':' +
+    date.getSeconds()
   );
 }
 
-function addCreatedPersonToList(person) {
-  localStorage.setItem(
-    localStorageKeys.USERS_TABLE,
-    JSON.stringify([...readUsers(), person])
-  );
+function getByClassName(param) {
+  return document.querySelector(`.${param}`);
 }
 
-function createPersonFromData(person) {
-  return {
-    firstName: person.name.first,
-    lastName: person.name.last,
-    registerDate: Date.parse(person.registered.date),
-    nationality: person.nat,
-    locationAdress: {
-      country: person.location.country,
-      city: person.location.city,
-      state: person.location.state,
-      postcode: person.location.postcode,
-      street: person.location.street.name,
-      houseNumber: person.location.street.number,
-    },
-    picture: {
-      large: person.picture.large,
-      medium: person.picture.medium,
-      thumbnail: person.picture.thumbnail,
-    },
-  };
+function appendElement(elem,className){
+  getByClassName(className).appendChild(elem);
 }
 
-function createPersonDiv() {
-  const personFieldBody = document.createElement("div");
-  personFieldBody.className = "person-container";
-  document.body.appendChild(personFieldBody);
-}
-
-function createPersonDataBody() {
-  const personDataBody = document.createElement("div");
-  personDataBody.className = "person-data";
-  document.querySelector(".person-container").appendChild(personDataBody);
-}
-
-function addFieldsToPersonData(createdPerson) {
-  addField("firstName", createdPerson.firstName);
-  addField("lastName", createdPerson.lastName);
-  addField(
-    "address",
-    `${createdPerson.locationAdress.country}<br>
-    ${createdPerson.locationAdress.city}<br>
-    ${createdPerson.locationAdress.state}<br>
-    ${createdPerson.locationAdress.postcode}<br>
-    ${createdPerson.locationAdress.street}<br>
-    ${createdPerson.locationAdress.houseNumber}`
-  );
-  addField("nationality", createdPerson.nationality);
-  addField("registeredDate", timestampToDate(createdPerson.registerDate));
+function appendElementToBody(elem){
+  document.body.appendChild(elem);
 }
--- src/table.js
+++ src/table.js
@@ -1,37 +1,43 @@
 let tableData = [];
 const localStorageKeys = {
-  USERS_TABLE: "usersTable",
+  DATA_TABLE: 'usersTable',
 };
-// Jeżeli wiem że jest tylko jedene taki element i nie ma więcej to: document.querySelector('#id' lub '.className')
-//Jeżeli wiem że jest więcej elementów to używam document.querySelectorAll() - zaznacza wiele elementów
-//Dobrze napisać osobną metodę do wrapowania z try catchem
+
+const classNames = {
+  USERS_TABLE: 'users-table',
+};
+
+/*  generateHeaders(tableBody);
+  appendDataToTable(tableBody);
+  document.body.appendChild(tableBody); */
 
 showTable();
 
 function showTable() {
   try {
     tableData = JSON.parse(
-      sessionStorage.getItem(localStorageKeys.USERS_TABLE)
+      sessionStorage.getItem(localStorageKeys.DATA_TABLE)
     );
   } catch (error) {
-    console.log("Failed to obtain data from session");
+    console.log('Failed to obtain data from session');
   }
-  generateTableContainer();
+  appendElementToBody(createTableContainer());
+  appendElement(createHeaders(),classNames.USERS_TABLE);
+  appendDataToTable()
 }
 
-function generateTableContainer() {
-  const tableBody = document.createElement("table");
-  generateHeaders(tableBody);
-  generateTableData(tableBody);
-  document.body.appendChild(tableBody);
+function createTableContainer() {
+  const tableBody = document.createElement('table');
+  tableBody.className = classNames.USERS_TABLE;
+  return tableBody;
 }
 
-function generateTableData(tableBody) {
+function appendDataToTable(tableBody) {
   for (i = tableData.length - 1; i >= 0; i--) {
-    const tableRow = document.createElement("tr");
+    const tableRow = createRow();
     tableRow.appendChild(createCell(tableData[i].firstName));
     tableRow.appendChild(createCell(tableData[i].lastName));
-    tableRow.appendChild(createCell(tableData[i].locationAdress.country));
+    tableRow.appendChild(createCell(tableData[i].locationAddress.country));
     tableRow.appendChild(
       createCell(timestampToDate(tableData[i].registerDate))
     );
@@ -40,41 +46,47 @@ function generateTableData(tableBody) {
 }
 
 function createCell(textInCell) {
-  const cell = document.createElement("td");
+  const cell = document.createElement('td');
   cell.innerHTML = textInCell;
   return cell;
 }
 
 function createHeaderCellWithFunction(textInCell, param) {
-  const cell = document.createElement("td");
-  cell.addEventListener("click", function () {
+  const cell = document.createElement('td');
+  cell.addEventListener('click', function(){
     sortTable(param);
   });
   cell.innerHTML = textInCell;
   return cell;
 }
 
-function generateHeaders(tableBody) {
-  const tableRow = document.createElement("tr");
-  tableRow.appendChild(createCell("First Name"));
-  tableRow.appendChild(createHeaderCellWithFunction("Last Name", "lastName"));
-  tableRow.appendChild(createCell("Country"));
-  tableRow.appendChild(createHeaderCellWithFunction("Register Date", "date"));
-  tableBody.appendChild(tableRow);
+function createRow(){
+  return document.createElement('tr');
+}
+
+function createHeaders() {
+  const headersRow = createRow();
+  headersRow.appendChild(createCell('First Name'));
+  headersRow.appendChild(createHeaderCellWithFunction('Last Name', 'lastName'));
+  headersRow.appendChild(createCell('Country'));
+  headersRow.appendChild(createHeaderCellWithFunction('Register Date', 'date'));
+  return headersRow;
 }
 
 function sortTable(param) {
-  if (param === "lastName") {
+  if (param === 'lastName') {
     tableData.sort(sortByLastName);
-    document.getElementsByTagName("table")[0].remove();
-    generateTableContainer();
+    getByClassName(classNames.USERS_TABLE).remove();
+    createTableContainer();
   }
-  if (param === "date") {
+  if (param === 'date') {
     tableData.sort(sortByDate);
-    document.getElementsByTagName("table")[0].remove();
-    generateTable();
+    getByClassName(classNames.USERS_TABLE).remove();
+    createTableContainer();
   }
 }
+
+//HELPERS
 
 function sortByLastName(a, b) {
   return b.lastName.localeCompare(a.lastName);
@@ -85,18 +97,30 @@ function sortByDate(a, b) {
 }
 
 function timestampToDate(param) {
-  let date = new Date(param);
+  const date = new Date(param);
   return (
     date.getDate() +
-    "/" +
+    '/' +
     (date.getMonth() + 1) +
-    "/" +
+    '/' +
     date.getFullYear() +
-    " " +
+    ' ' +
     date.getHours() +
-    ":" +
+    ':' +
     date.getMinutes() +
-    ":" +
+    ':' +
     date.getSeconds()
   );
+}
+
+function getByClassName(param) {
+  return document.querySelector(`.${param}`);
+}
+
+function appendElement(elem,className){
+  getByClassName(className).appendChild(elem);
+}
+
+function appendElementToBody(elem){
+  document.body.appendChild(elem);
 }
